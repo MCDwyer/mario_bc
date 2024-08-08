479f52d091682023b0c04b65e10a323d154115aa
Madeleine Dwyer <mdwyer@turing.ac.uk>
2024-08-08 14:26:44 +0100

    refactor code to have constants outside function

--- unsupervised_training.py
+++ unsupervised_training.py
@@ -11,42 +11,40 @@ import sys
 
 TIMESTEPS = 100
 UNSUPERVISED = True
+MODEL_NAME = "PPO"
+MODEL_CLASS = PPO
+POLICY = "CnnPolicy"
+TRAINING_DATA_NAME = "amalgam"
+LEVEL_CHANGE = "random"
+
+# training_data_name = "expert_dist"
+# training_data_name = "nonexpert_dist"
+
+# training_data_name = "expert_score"
+# training_data_name = "nonexpert_score"
+
+# training_data_name = "slower"
+# training_data_name = "faster"
 
 def main(agent_index):
-    model_name = "PPO"
-    model_class = PPO
-    policy = "CnnPolicy"
-
-    training_data_name = "amalgam"
-
-    training_data_name = "expert_dist"
-    training_data_name = "nonexpert_dist"
-
-    training_data_name = "expert_score"
-    training_data_name = "nonexpert_score"
-
-    training_data_name = "slower"
-    training_data_name = "faster"
-
-    level_change = "random"
 
     register(
             id='MarioEnv-v0',
             entry_point='GymEnvs.retro_env_wrapper:MarioEnv',
         )
 
-    log_dir = f"training_logs/level_change_{level_change}/"
+    log_dir = f"training_logs/level_change_{LEVEL_CHANGE}/"
 
     if UNSUPERVISED:
         log_dir = f"{log_dir}unsupervised/"
     else:
-        log_dir = f"{log_dir}supervised/{training_data_name}/"
+        log_dir = f"{log_dir}supervised/{TRAINING_DATA_NAME}/"
 
     env = gym.make('MarioEnv-v0')
 
     string_timesteps = f"{int(TIMESTEPS/1000)}k"
 
-    model = model_class(policy, env, verbose=1, tensorboard_log=log_dir)
+    model = MODEL_CLASS(POLICY, env, verbose=1, tensorboard_log=log_dir)
 
     # Configure logging
     tmp_path = log_dir + "/tmp/"
@@ -54,9 +52,9 @@ def main(agent_index):
     model.set_logger(new_logger)
 
     if UNSUPERVISED:
-        name_prefix = f"unsupervised_{model_name}_{agent_index}"
+        name_prefix = f"unsupervised_{MODEL_NAME}_{agent_index}"
     else:
-        name_prefix = f"supervised_{model_name}_with_{training_data_name}_{agent_index}"
+        name_prefix = f"supervised_{MODEL_NAME}_with_{TRAINING_DATA_NAME}_{agent_index}"
 
     # Define callbacks
     eval_callback = EvalCallback(env, best_model_save_path=log_dir,
@@ -68,17 +66,17 @@ def main(agent_index):
 
     # Train the model
     model.learn(total_timesteps=TIMESTEPS, callback=[eval_callback, checkpoint_callback])
-    model.save(log_dir + f"{string_timesteps}_{model_name}_{agent_index}")
+    model.save(log_dir + f"{string_timesteps}_{MODEL_NAME}_{agent_index}")
 
     # save the history from the env
-    history_json_filepath = log_dir + f"{string_timesteps}_{model_name}_{agent_index}.json"
+    history_json_filepath = log_dir + f"{string_timesteps}_{MODEL_NAME}_{agent_index}.json"
     with open(history_json_filepath, "w") as outfile:
         json.dump(env.history, outfile)
 
     # env.set_record_option("test_bk2s/.")
 
     # Load the trained agent to check it's saved properly
-    model = model_class.load(log_dir + f"{string_timesteps}_{model_name}_{agent_index}")
+    model = MODEL_CLASS.load(log_dir + f"{string_timesteps}_{MODEL_NAME}_{agent_index}")
 
     # Run a 1000 timesteps to generate a gif just as a check measure (not actual evaluation)
     # Create a figure and axis
@@ -112,9 +110,9 @@ def main(agent_index):
     if UNSUPERVISED:
         animation_path += "unsupervised"
     else:
-        animation_path += f"supervised_w_{training_data_name}"
+        animation_path += f"supervised_w_{TRAINING_DATA_NAME}"
 
-    animation_path += f"_{model_name}_{string_timesteps}_{agent_index}.gif"
+    animation_path += f"_{MODEL_NAME}_{string_timesteps}_{agent_index}.gif"
     ani.save(animation_path, writer='pillow', fps=100)
 
     # # Show the animation
@@ -127,6 +125,6 @@ if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: python unsupervised_training.py <index_number>")
         sys.exit(1)
-    
+
     agent_index = sys.argv[1]
     main(agent_index)
