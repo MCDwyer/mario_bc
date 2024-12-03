d2e85ca8236b8494e4467323bab8e6d945343be4
Madeleine Dwyer <mdwyer@turing.ac.uk>
2024-12-03 14:43:34 +0000

    conda setup update

--- conda_setup.sh
+++ conda_setup.sh
@@ -2,7 +2,8 @@ conda create --force -n mario_bc_env python=3.7.2
 
 conda run -n mario_bc_env python --version
 
-conda run -n mario_bc_env pip install setuptools==41.2.0
+# conda install -c conda-forge setuptools=41.2.0 numpy pandas torch
+
 conda run -n mario_bc_env pip install numpy
 conda run -n mario_bc_env pip install pandas
 conda run -n mario_bc_env pip install torch
@@ -12,10 +13,12 @@ conda run -n mario_bc_env pip install scikit-learn
 conda run -n mario_bc_env pip install matplotlib
 conda run -n mario_bc_env pip install stable-baselines3
 conda run -n mario_bc_env pip install gymnasium
-conda run -n mario_bc_env pip install opencv
+conda run -n mario_bc_env pip install opencv-python==4.10.0.84
+conda run -n mario_bc_env pip install opencv-python-headless==4.10.0.84
 conda run -n mario_bc_env pip install optuna
 conda run -n mario_bc_env pip install tensorboard
 conda run -n mario_bc_env pip install scipy
 conda run -n mario_bc_env pip install gym==0.25.2
 conda run -n mario_bc_env pip install gym-retro==0.8.0
 conda run -n mario_bc_env pip install kaleido
+conda run -n mario_bc_env pip install setuptools==41.2.0
