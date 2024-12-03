f2cd516bb9309212e8b1366e5c8fb0bef6254484
Madeleine Dwyer <mdwyer@turing.ac.uk>
2024-12-03 13:05:44 +0000

    conda environment setup

---
+++ conda_setup.sh
@@ -0,0 +1,20 @@
+
+source activate mario_bc_env
+
+pip install setuptools==41.2.0
+pip install numpy
+pip install pandas
+pip install pytorch
+pip install plotly
+pip install nbformat
+pip install scikit-learn
+pip install matplotlib
+pip install stable-baselines3
+pip install gymnasium
+pip install opencv
+pip install optuna
+pip install tensorboard
+pip install scipy
+pip install gym==0.25.2
+pip install gym-retro==0.8.0
+pip install kaleido
