5ebfcd6b1d656c92a3039e8e19d42b22c20fa873
Madeleine Dwyer <mdwyer@turing.ac.uk>
2024-12-03 13:51:36 +0000

    conda setup

--- conda_setup.sh
+++ conda_setup.sh
@@ -1,20 +1,21 @@
+conda create --force -n mario_bc_env python=3.7.2
 
-source activate mario_bc_env
+conda run -n mario_bc_env python --version
 
-pip install setuptools==41.2.0
-pip install numpy
-pip install pandas
-pip install pytorch
-pip install plotly
-pip install nbformat
-pip install scikit-learn
-pip install matplotlib
-pip install stable-baselines3
-pip install gymnasium
-pip install opencv
-pip install optuna
-pip install tensorboard
-pip install scipy
-pip install gym==0.25.2
-pip install gym-retro==0.8.0
-pip install kaleido
+conda run -n mario_bc_env pip install setuptools==41.2.0
+conda run -n mario_bc_env pip install numpy
+conda run -n mario_bc_env pip install pandas
+conda run -n mario_bc_env pip install pytorch
+conda run -n mario_bc_env pip install plotly
+conda run -n mario_bc_env pip install nbformat
+conda run -n mario_bc_env pip install scikit-learn
+conda run -n mario_bc_env pip install matplotlib
+conda run -n mario_bc_env pip install stable-baselines3
+conda run -n mario_bc_env pip install gymnasium
+conda run -n mario_bc_env pip install opencv
+conda run -n mario_bc_env pip install optuna
+conda run -n mario_bc_env pip install tensorboard
+conda run -n mario_bc_env pip install scipy
+conda run -n mario_bc_env pip install gym==0.25.2
+conda run -n mario_bc_env pip install gym-retro==0.8.0
+conda run -n mario_bc_env pip install kaleido
