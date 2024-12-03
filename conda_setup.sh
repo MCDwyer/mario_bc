ba25471ef6a16f0eb96ac34d069630da78e0863a
Madeleine Dwyer <mdwyer@turing.ac.uk>
2024-12-03 15:06:57 +0000

    conda env set up

--- conda_setup.sh
+++ conda_setup.sh
@@ -1,24 +1,25 @@
-conda create --force -n mario_bc_env python=3.7.2
+conda env create --force -f environment.yml
 
 conda run -n mario_bc_env python --version
 
 # conda install -c conda-forge setuptools=41.2.0 numpy pandas torch
 
-conda run -n mario_bc_env pip install numpy
-conda run -n mario_bc_env pip install pandas
-conda run -n mario_bc_env pip install torch
-conda run -n mario_bc_env pip install plotly
-conda run -n mario_bc_env pip install nbformat
-conda run -n mario_bc_env pip install scikit-learn
-conda run -n mario_bc_env pip install matplotlib
-conda run -n mario_bc_env pip install stable-baselines3
-conda run -n mario_bc_env pip install gymnasium
+# conda run -n mario_bc_env pip --version
+# conda run -n mario_bc_env pip install -U pip setuptools==41.2.0
+conda run -n mario_bc_env pip install numpy==1.21.6
+conda run -n mario_bc_env pip install pandas==1.3.5
+conda run -n mario_bc_env pip install torch==1.13.1
+conda run -n mario_bc_env pip install plotly==5.18.0
+conda run -n mario_bc_env pip install nbformat==5.8.0
+conda run -n mario_bc_env pip install scikit-learn==1.0.2
+conda run -n mario_bc_env pip install matplotlib==3.5.3
+conda run -n mario_bc_env pip install stable-baselines3==2.0.0
+conda run -n mario_bc_env pip install gymnasium==0.28.1
 conda run -n mario_bc_env pip install opencv-python==4.10.0.84
 conda run -n mario_bc_env pip install opencv-python-headless==4.10.0.84
-conda run -n mario_bc_env pip install optuna
-conda run -n mario_bc_env pip install tensorboard
-conda run -n mario_bc_env pip install scipy
+conda run -n mario_bc_env pip install optuna==3.6.1
+conda run -n mario_bc_env pip install tensorboard==2.11.2
+conda run -n mario_bc_env pip install scipy==1.7.3
 conda run -n mario_bc_env pip install gym==0.25.2
 conda run -n mario_bc_env pip install gym-retro==0.8.0
-conda run -n mario_bc_env pip install kaleido
-conda run -n mario_bc_env pip install setuptools==41.2.0
+conda run -n mario_bc_env pip install kaleido==0.2.1
--- tuning.sh
+++ tuning.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # bash script to run unsupervised tuning
 
-nohup python -u model_tuning.py None PPO > nohup/tuning_PPO.out 2>&1 &
+conda run -n mario_bc_env nohup python -u model_tuning.py None PPO > nohup/tuning_PPO.out 2>&1 &
 echo "Started unsupervised PPO tuning, output to nohup/tuning_PPO.out"
 
-nohup python -u model_tuning.py None DQN > nohup/tuning_DQN.out 2>&1 &
+conda run -n mario_bc_env nohup python -u model_tuning.py None DQN > nohup/tuning_DQN.out 2>&1 &
 echo "Started unsupervised DQN tuning, output to nohup/tuning_DQN.out"
 
-nohup python -u model_tuning.py None SAC > nohup/tuning_SAC.out 2>&1 &
+conda run -n mario_bc_env nohup python -u model_tuning.py None SAC > nohup/tuning_SAC.out 2>&1 &
 echo "Started unsupervised SAC tuning, output to nohup/tuning_SAC.out"
