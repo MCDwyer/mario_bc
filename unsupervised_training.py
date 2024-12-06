6a19c5783ede72ea752adc7cea30fb67af8f78ed
Madeleine Dwyer <mdwyer@turing.ac.uk>
2024-12-06 16:24:48 +0000

    updating training py and cleaning it up, setting up slurm files and batch script

--- unsupervised_training.py
+++ unsupervised_training.py
@@ -6,9 +6,10 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from stable_baselines3.common.callbacks import EvalCallback, CheckpointCallback
 from stable_baselines3.common.logger import configure
+from stable_baselines3.common.vec_env import DummyVecEnv, VecFrameStack
+from stable_baselines3.common.monitor import Monitor
 import json
 import sys
-import torch
 import time
 import psutil
 import os
@@ -21,8 +22,14 @@ RETRAINING = False
 POLICY = "CnnPolicy"
 LEVEL_CHANGE = "random"
 NUM_ACTIONS = 13
+ONLY_BC = True
 
 EVALUATION_FREQ = 100000
+SAVE_FREQ = 500000
+
+EXP_RUN_ID = "NO_EXPERIMENT_ID_SET"
+
+MODEL_PARAMETERS = {}
 
 class ActionDistributionEvalCallback(EvalCallback):
     def __init__(self, eval_env, eval_freq, n_eval_episodes=5, verbose=0, **kwargs):
@@ -79,6 +86,7 @@ def test_ani(env, model, string_timesteps):
     im = ax.imshow(np.zeros((84, 84)), cmap='gray', vmin=0, vmax=255)
 
     global OBS
+    # OBS, _ = env.reset(options={"level": "Level1-1", "unprocessed_obs": True})
     OBS, _ = env.reset(options={"level": "Level1-1"})
 
     # Function to update the image
@@ -107,41 +115,18 @@ def test_ani(env, model, string_timesteps):
     animation_path += f"_{MODEL_NAME}_{string_timesteps}_{agent_index}.gif"
     ani.save(animation_path, writer='pillow', fps=100)
 
-    # # Show the animation
-    # plt.show() 
-
     env.close()
 
-def compare_params(dict1, dict2): 
-    for key in dict1.keys(): 
-        if not torch.equal(dict1[key], dict2[key]): 
-            return False 
-        
-    return True
+# def ppo_model(env, log_dir):
 
-def ppo_model(env, log_dir):
-
-    if UNSUPERVISED:
-        params = {'rl_learning_rate': 0.0004256977228617011, 'n_steps': 3588, 'gamma': 0.9244431355829242, 'gae_lambda': 0.901757161292598, 'ent_coef': 0.007033089897296567, 'clip_range': 0.31361009245800875, 'vf_coef': 0.824772120758773}
-    else:
-        params = {'learning_rate': 0.0009422578032986744, 'n_epochs': 15, 'batch_size': 1043, 'rl_learning_rate': 0.00019810071043939884, 'n_steps': 608, 'gamma': 0.9632436461255943, 'gae_lambda': 0.8365664231014514, 'ent_coef': 0.0014620665698483156, 'clip_range': 0.2873084366251664, 'vf_coef': 0.20790175758482327}
+#     if UNSUPERVISED:
+#         params = {'rl_learning_rate': 0.0004256977228617011, 'n_steps': 3588, 'gamma': 0.9244431355829242, 'gae_lambda': 0.901757161292598, 'ent_coef': 0.007033089897296567, 'clip_range': 0.31361009245800875, 'vf_coef': 0.824772120758773}
+#     else:
+#         params = {'learning_rate': 0.0009422578032986744, 'n_epochs': 15, 'batch_size': 1043, 'rl_learning_rate': 0.00019810071043939884, 'n_steps': 608, 'gamma': 0.9632436461255943, 'gae_lambda': 0.8365664231014514, 'ent_coef': 0.0014620665698483156, 'clip_range': 0.2873084366251664, 'vf_coef': 0.20790175758482327}
 
 
-    model = PPO(POLICY,
-                env,
-                batch_size=params['n_steps'],
-                learning_rate=params['rl_learning_rate'],
-                n_steps=params['n_steps'],
-                gamma=params['gamma'],
-                gae_lambda=params['gae_lambda'],
-                ent_coef=params['ent_coef'],
-                clip_range=params['clip_range'],
-                vf_coef=params['vf_coef'],
-                verbose=1,
-                tensorboard_log=log_dir,
-                )
-    
-    return model, params
+def set_model_parameters():
+    return
 
 def main(agent_index):
 
@@ -150,89 +135,62 @@ def main(agent_index):
             entry_point='GymEnvs.retro_env_wrapper:MarioEnv',
         )
 
-    log_dir = f"training_logs/level_change_{LEVEL_CHANGE}/"
+    log_dir = f"experiments/{EXP_RUN_ID}/training_logs/level_change_{LEVEL_CHANGE}/"
+    model_dir = f"experiments/{EXP_RUN_ID}/saved_models/level_change_{LEVEL_CHANGE}/"
 
     if UNSUPERVISED:
         log_dir = f"{log_dir}unsupervised/"
     else:
         log_dir = f"{log_dir}supervised/{TRAINING_DATA_NAME}/"
 
+    os.makedirs(log_dir, exist_ok=True)
+    os.makedirs(model_dir, exist_ok=True)
+
     env = gym.make('MarioEnv-v0')
 
-    # env = Monitor(env)
-    # env = DummyVecEnv([lambda: env])
+    env = Monitor(env)
+    env = DummyVecEnv([lambda: env])
 
-    # # Stack frames to allow temporal information to be captured
-    # env = VecFrameStack(env, n_stack=1)
-
-    string_timesteps = f"{int(TIMESTEPS/1000)}k"
-
-    model_path = log_dir + f"{string_timesteps}_{MODEL_NAME}_{agent_index}"
-
-    if RETRAINING:
-        previous_timestep_string = f"{int((TIMESTEPS-TIMESTEP_INCREMENT)/1000)}k"
-        previous_model_path = log_dir + f"{previous_timestep_string}_{MODEL_NAME}_{agent_index}"
-
-        model = MODEL_CLASS.load(previous_model_path, env, verbose=1, tensorboard_log=log_dir)
+    if MODEL_PARAMETERS:
+        env = VecFrameStack(env, n_stack=MODEL_PARAMETERS['n_stack'])
     else:
-        # if MODEL_CLASS == PPO:
-        #     model, params = ppo_model(env, log_dir)
-        # else:
-        #     model, params = MODEL_CLASS(POLICY, env, verbose=1, tensorboard_log=log_dir)
+        # Stack frames to allow temporal information to be captured
+        env = VecFrameStack(env, n_stack=1)
 
-        if MODEL_NAME == "DQN":
-            model = MODEL_CLASS(POLICY, env, verbose=1, tensorboard_log=log_dir, buffer_size=450000)
-        else:
-            # model = MODEL_CLASS(POLICY, env, verbose=1, tensorboard_log=log_dir)
-            params = {'learning_rate': 0.0009422578032986744, 'n_epochs': 15, 'batch_size': 1043, 'rl_learning_rate': 0.00019810071043939884, 'n_steps': 608, 'gamma': 0.9632436461255943, 'gae_lambda': 0.8365664231014514, 'ent_coef': 0.0014620665698483156, 'clip_range': 0.2873084366251664, 'vf_coef': 0.20790175758482327}
-# model = PPO(CustomCnnPolicy, env, verbose=1)
-            model = PPO(POLICY,
-                env,
-                batch_size=params['n_steps'],
-                learning_rate=params['rl_learning_rate'],
-                n_steps=params['n_steps'],
-                gamma=params['gamma'],
-                gae_lambda=params['gae_lambda'],
-                ent_coef=params['ent_coef'],
-                clip_range=params['clip_range'],
-                vf_coef=params['vf_coef'],
-                verbose=1,
-                tensorboard_log=log_dir,
-                )
+    string_timesteps = f"{int(TIMESTEPS/1000000)}M"
+
+    sup_string = "unsupervised" if UNSUPERVISED else f"bc_{TRAINING_DATA_NAME}"
+    model_path = model_dir + f"{MODEL_NAME}_{sup_string}_{string_timesteps}_agent_{agent_index}"
+
+    if MODEL_PARAMETERS:
+        model = set_model_parameters()
+
+    else:
+        model = MODEL_CLASS(POLICY, env, verbose=1, tensorboard_log=log_dir)
 
         if not UNSUPERVISED:
-            # Save initial model weights before behavior cloning
-            bc_model_path = f"{model_path}_bc"
-            model.save(f"{bc_model_path}_initial_weights")
+            bc_model_path = f"{model_path}_bc_only"
 
-            print(model.policy)
-
-            # bc_model, bc_model_path = behavioural_cloning.behavioural_cloning_with_imitation(env, model_path, TRAINING_FILEPATH)
-            # model = MODEL_CLASS.load(bc_model_path, env, verbose=1, tensorboard_log=log_dir)
-            # print(env.action_space)
-            print("behaviour cloning starting")
-            model = behavioural_cloning.behavioural_cloning(MODEL_NAME, model, env, TRAINING_FILEPATH, bc_model_path) #, params["learning_rate"], params["n_epochs"], params["batch_size"])
-            
-            initial_model = PPO.load(f"{bc_model_path}_initial_weights", env=env)
-            bc_trained_model = PPO.load(bc_model_path, env=env)
-            
-            policy_params_before = initial_model.policy.state_dict()
-            
-            # Access the updated policy network parameters
-            policy_params_after = bc_trained_model.policy.state_dict()
-            
-            # Compare specific weights (e.g., first layer weights)
-            if compare_params(policy_params_before, policy_params_after):
-                print("Weights have not changed.")
+            # check if bc_model exists
+            if os.path.exists(model_path):
+                model = MODEL_CLASS.load(bc_model_path)
+                print("BC Trained model loaded successfully!")
             else:
-                print("Weights have been updated!")
+                print(f"Model file '{bc_model_path}' does not exist.")
+                model = behavioural_cloning.behavioural_cloning(MODEL_NAME, model, env, TRAINING_FILEPATH, bc_model_path, lr=model.learning_rate, num_epochs=model.n_epochs, batch_size=model.batch_size) #, params["learning_rate"], params["n_epochs"], params["batch_size"])
+                if ONLY_BC:
+                    sys.exit()
 
-    # print(model.policy)
+    print("RL Training Info")
+    print(f"Model: {MODEL_NAME}, BC dataset: {TRAINING_DATA_NAME}, timesteps: {TIMESTEPS}")
+    print(f"Model Parameters Used: {MODEL_PARAMETERS}\n")
+    print(f"Model Parameters (stored on model): {model.policy_kwargs}\n")
 
-    if UNSUPERVISED:
-        name_prefix = f"unsupervised_{MODEL_NAME}_{string_timesteps}_{agent_index}"
-    else:
-        name_prefix = f"supervised_{MODEL_NAME}_with_{TRAINING_DATA_NAME}_{string_timesteps}_{agent_index}"
+    print("Model Policy Structure:")
+    print(model.policy)
+    print()
+
+    name_prefix = f"{MODEL_NAME}_{string_timesteps}_agent_{agent_index}"
 
     # Configure logging
     tmp_path = log_dir + f"/{name_prefix}/"
@@ -241,23 +199,20 @@ def main(agent_index):
 
     test_ani(env, model, "pre_unsupervised_training")
 
-    # Define callbacks
-    # eval_callback = EvalCallback(env, best_model_save_path=f"{log_dir}{name_prefix}/",
-    #                             log_path=log_dir, eval_freq=100000,
-    #                             deterministic=True, render=False)    
     eval_callback = ActionDistributionEvalCallback(env, 
-                                                   eval_freq=100000, 
+                                                   eval_freq=EVALUATION_FREQ, 
                                                    verbose=1, 
                                                    best_model_save_path=f"{log_dir}{name_prefix}/", 
                                                    log_path=log_dir)
 
-    checkpoint_callback = CheckpointCallback(save_freq=500000, save_path=log_dir,
+    checkpoint_callback = CheckpointCallback(save_freq=SAVE_FREQ, save_path=log_dir,
                                             name_prefix=name_prefix)
 
     # Train the model
-    timesteps = TIMESTEP_INCREMENT if RETRAINING else TIMESTEPS
-    model.learn(total_timesteps=timesteps, callback=[eval_callback, checkpoint_callback])
+    # timesteps = TIMESTEP_INCREMENT if RETRAINING else TIMESTEPS
+    model.learn(total_timesteps=TIMESTEPS, callback=[eval_callback, checkpoint_callback])
     model.save(model_path)
+    print(f"Training finished and model saved to {model_path}.")
 
     # save the history from the env
     history_json_filepath = f"{model_path}.json"
@@ -274,8 +229,9 @@ def main(agent_index):
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
+    if len(sys.argv) < 4:
         print("Usage: python unsupervised_training.py <index_number> <training_data_name> <model>")
+        print("OR: python unsupervised_training.py <index_number> <training_data_name> <model> <experiment_run_id>")
         sys.exit(1)
 
     # Start timing
@@ -284,6 +240,10 @@ if __name__ == "__main__":
     agent_index = sys.argv[1]
     TRAINING_DATA_NAME = sys.argv[2]
     MODEL_NAME = sys.argv[3]
+
+    if len(sys.argv) == 5:
+        EXP_RUN_ID = sys.argv[4]
+        EXP_RUN_ID.replace(" ", "_")
 
     if TRAINING_DATA_NAME == 'None':
         UNSUPERVISED = True
@@ -300,7 +260,7 @@ if __name__ == "__main__":
     else:
         MODEL_CLASS = SAC
 
-    print(f"Starting training for agent {agent_index} with model type: {MODEL_NAME} and using {TRAINING_DATA_NAME} training data.")
+    print(f"Starting training for agent {agent_index} with model type: {MODEL_NAME} and using {TRAINING_DATA_NAME} training data.\n\n")
 
     main(agent_index)
 
