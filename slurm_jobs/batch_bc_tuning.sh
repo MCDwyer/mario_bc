02bb0c6143c989282d54f2e42612e1c628b53a50
Madeleine Dwyer <mdwyer@turing.ac.uk>
2024-12-06 17:15:01 +0000

    fixing error in slurm file

--- slurm_jobs/batch_bc_tuning.sh
+++ slurm_jobs/batch_bc_tuning.sh
@@ -13,6 +13,6 @@ BC_DATA_OPTIONS="amalgam expert_distance nonexpert_distance"
 
 # Iterate over the list
 for item in $BC_DATA_OPTIONS; do
-    JOB_NAME="BC_TUNING_$item_$MODEL_TYPE"
-    sbatch --job-name=$JOB_NAME --output="${JOB_NAME}.out" --error="${JOB_NAME}.err" bc_tuning.slurm $item $MODEL_TYPE
+    JOB_NAME="BC_TUNING_${item}_${MODEL_TYPE}"
+    sbatch --job-name=$JOB_NAME --output="${JOB_NAME}.out" --error="${JOB_NAME}.err" bc_tuning.slurm ${item} ${MODEL_TYPE}
 done
