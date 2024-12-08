336caf2f91cecc8b5cfd33d116898cc8c0ac5416
Madeleine Dwyer <mdwyer@turing.ac.uk>
2024-12-08 20:25:52 +0000

    updating bash script for unsup tuning

--- slurm_jobs/batch_unsup_tuning.sh
+++ slurm_jobs/batch_unsup_tuning.sh
@@ -2,12 +2,18 @@
 # bash script to sbatch bc tuning
 
 if [ "$#" -ne 1 ]; then
-  echo "Usage: batch_unsup_tuning.sh <model_type> <bc_data>"
+  echo "Usage: batch_unsup_tuning.sh <bc_data>"
   exit 1
 fi
 
-MODEL_TYPE=$1
-BC_DATA=$2
+BC_DATA=$1
 
-JOB_NAME="TUNING_${MODEL_TYPE}_${BC_DATA}"
-sbatch --job-name=$JOB_NAME --output="${JOB_NAME}.out" --error="${JOB_NAME}.err" unsup_tuning.slurm $MODEL_TYPE $BC_DATA
+# Define a space-separated list
+MODEL_NAMES="PPO DQN SAC"
+
+# Iterate over the list
+for MODEL_TYPE in $MODEL_NAMES; do
+    JOB_NAME="TUNING_${MODEL_TYPE}_${BC_DATA}"
+    sbatch --job-name=$JOB_NAME --output="${JOB_NAME}.out" --error="${JOB_NAME}.err" unsup_tuning.slurm $MODEL_TYPE $BC_DATA
+done
+
