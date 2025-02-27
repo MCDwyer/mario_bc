3421cb24bd6392fc9db8d39fe8b9baa3a993d214
M.C.Dwyer <mcd2g19@cyan51.cluster.local>
2025-02-27 16:56:27 +0000

    updating files, misc reasons as I haven't kept up with commits

--- evaluate_agents.py
+++ evaluate_agents.py
@@ -1,4 +1,4 @@
-from stable_baselines3 import PPO
+from stable_baselines3 import PPO, DQN, SAC
 import gymnasium as gym
 from gymnasium.envs.registration import register
 import numpy as np
@@ -9,6 +9,10 @@ from scipy.stats import mannwhitneyu, shapiro, levene, ttest_ind, ttest_rel, wil
 # from statsmodels.stats.multicomp import pairwise_tukeyhsd
 import pickle
 import os
+import json
+import shutil
+import copy
+import pandas as pd
 
 TIMESTEPS = 20000000
 UNSUPERVISED = False
@@ -32,54 +36,145 @@ TRAINING_FILEPATH += TRAINING_DATA_NAME + "_bc_data.obj"
 # training_data_name = "faster"
 
 ALL_LEVELS = ["Level1-1", "Level2-1", "Level3-1", "Level4-1", "Level5-1", "Level6-1", "Level7-1", "Level8-1"]
-TEST_EPISODES = 1000
+TEST_EPISODES = 100#0
 AGENT_INDICES = list(range(5))
+MAX_Y = 768
 
-def evaluate_model(model, env, level, record_dir, n_episodes=10):
-    """
-    Evaluates the model by running it in the environment for n_episodes.
-    Returns the mean reward over these episodes.
-    """
-    episode_rewards = []
-    episode_trajectories = []
-    episode_info = []
-    episode_actions = []
-    total_actions = np.zeros(env.action_space.n)
+def evaluate_model(model, env, level, n_episodes=10):
+
+    all_trajectories = []
+    all_actions = []
+    all_action_distributions = []
+    all_dist_rewards = []
+    all_score_rewards = []
+    all_combined_rewards = []
 
     for i in range(n_episodes):
-        record_path = record_dir + f"/episode_{i}/"
-        os.makedirs(record_path, exist_ok=True)
-
-        obs, _ = env.reset(options={"level": level, "record_option": record_path})
-        done = False
-        total_reward = 0.0
         trajectory = []
-        scores = []
-        actions = np.zeros(env.action_space.n)
+        action_distribution = np.zeros(13, dtype=int)
+        actions = []
+        total_dist_reward = 0
+        total_score_reward = 0
+        total_combined_reward = 0
+        total_reward = 0
+
+        step = 0
+
+        prev_position = 40
+        prev_score = 0
+        
+        obs, _ = env.reset(options={"level": level})
+        done = False
+
+        state_change = False
 
         while not done:
+            # step += 1
+
+            # if step > 10:
+            #     break
             action, _states = model.predict(obs)
             obs, reward, done, _, info = env.step(int(action))
 
-            actions[int(action)] += 1
+            if done:
+                break  # Exit immediately
+
+            # action_distribution[int(action)] += 1
+            np.add.at(action_distribution, int(action), 1)
 
             x = info["x_frame"]*256 + info["x_position_in_frame"]
             y = ((info["y_frame"]*256) + info["y_position_in_frame"])
+            
+            if info["player_state"] == 11 or level not in env.retro_env.statename or y > MAX_Y:# < -432:# or info["viewport_position"] > 1: #y < -432:# or info["player_dead"] != 32:# or y < -432:
+                break
+
             trajectory.append([x, y])
-            scores.append(info["score"])
+            actions.append(action)
+
+            # if state_change:
+            #     dist_reward = 0
+            #     score_reward = 0
+            # else:
+            #     dist_reward = (x - prev_position)
+            #     score_reward = (info['score'] - prev_score)
+            
+            dist_reward = env.dist_reward
+            score_reward = env.score_reward
+            total_dist_reward += dist_reward
+            total_score_reward += score_reward
+            total_combined_reward += (dist_reward/2 + score_reward/2)
 
             total_reward += reward
 
-        print(f"Episode {i} reward: {total_reward}")
-        episode_rewards.append(total_reward)
-        episode_trajectories.append(np.array(trajectory))
-        episode_info.append({'score': np.max(np.array(scores)), 'max_dist': np.max(np.array(trajectory)[:, 0]), 'num_timesteps': len(trajectory)})
-        episode_actions.append(actions)
-        total_actions += actions
+            prev_position = x
+            prev_score = info['score']
 
-    mean_reward = sum(episode_rewards) / n_episodes
+            if dist_reward < -2000:
+                print(reward, score_reward, dist_reward)
+                print(f"Why isn't this working!!!! player state = {info['player_state']}")
 
-    return mean_reward, episode_rewards, episode_trajectories, episode_info, episode_actions
+        all_trajectories.append(trajectory)
+        all_actions.append(actions)
+        all_action_distributions.append(action_distribution)
+        all_dist_rewards.append(total_dist_reward)
+        all_score_rewards.append(total_score_reward)
+        all_combined_rewards.append(total_combined_reward)
+
+        print(f"Episode {i} reward: {total_reward} - dist_reward: {total_dist_reward}")
+
+    print(f"Distance Reward: {all_dist_rewards}")
+    print(f"Score Reward: {all_score_rewards}")
+    print(f"Combined Reward: {all_combined_rewards}")
+
+    return all_trajectories, all_actions, all_action_distributions, all_dist_rewards, all_score_rewards, all_combined_rewards
+
+
+# def evaluate_model(model, env, level, record_dir, n_episodes=10):
+#     """
+#     Evaluates the model by running it in the environment for n_episodes.
+#     Returns the mean reward over these episodes.
+#     """
+#     episode_rewards = []
+#     episode_trajectories = []
+#     episode_info = []
+#     episode_actions = []
+#     total_actions = np.zeros(env.action_space.n)
+
+#     for i in range(n_episodes):
+#         record_path = record_dir + f"/episode_{i}/"
+#         os.makedirs(record_path, exist_ok=True)
+
+#         obs, _ = env.reset(options={"level": level, "record_option": record_path})
+#         done = False
+#         total_reward = 0.0
+#         trajectory = []
+#         scores = []
+#         actions = np.zeros(env.action_space.n)
+
+#         # print(env.retro_env.statename)
+#         while not done:
+#             action, _states = model.predict(obs)
+#             obs, reward, done, _, info = env.step(int(action))
+
+#             actions[int(action)] += 1
+
+#             x = info["x_frame"]*256 + info["x_position_in_frame"]
+#             y = ((info["y_frame"]*256) + info["y_position_in_frame"])
+#             trajectory.append([x, y])
+#             scores.append(info["score"])
+
+#             total_reward += reward
+
+#         print(f"Episode {i} reward: {total_reward}")
+#         episode_rewards.append(total_reward)
+#         episode_trajectories.append(trajectory)
+#         episode_info.append({'score': np.max(np.array(scores)), 'max_dist': np.max(np.array(trajectory)[:, 0]), 'num_timesteps': len(trajectory)})
+#         episode_actions.append(list(actions))
+#         total_actions += actions
+
+#     mean_reward = sum(episode_rewards) / n_episodes
+
+#     return mean_reward, episode_rewards, episode_trajectories, episode_info, episode_actions
 
 def get_results(model, env):
     mean_rewards = {}
@@ -166,33 +261,63 @@ def four_agent_statistical_tests(agent_1, agent_2, agent_3, agent_4, mean_reward
     print()
 
 def get_model_results(model_path, model_class, env):
-    eval_dir = model_path.split(".")[0] + "/evaluation/"
+    # eval_dir = model_path.split(".")[0] + "/evaluation/"
 
-    os.makedirs(eval_dir, exist_ok=True)
+    os.makedirs(model_path.split(".")[0], exist_ok=True)
 
     model = model_class.load(model_path, env, verbose=1)
 
-    level_results = {}
+    # level_results = {}
 
     for level in ALL_LEVELS:
-        record_path = eval_dir + f"{level}/bk2_files" 
-        os.makedirs(record_path, exist_ok=True)
+        # record_path = eval_dir + f"{level}/bk2_files" 
+        # os.makedirs(record_path, exist_ok=True)
 
         print(f"\tEvaluating on {level} for {TEST_EPISODES} episodes.")
 
-        mean_reward, rewards, episode_trajectories, episode_info = evaluate_model(model, env, level, record_path, TEST_EPISODES)
-
-        print(f"\tMean reward is {mean_reward}, bk2 files stored in {record_path}.\n")
-        level_results[level] = {"mean_reward": mean_reward, "episode_data": []}
+        # mean_reward, rewards, episode_trajectories, episode_info, episode_actions = evaluate_model(model, env, level, record_path, TEST_EPISODES)
+        all_trajectories, all_actions, all_action_distributions, all_dist_rewards, all_score_rewards, all_combined_rewards = evaluate_model(model, env, level, TEST_EPISODES)
         
-        for i, episode_reward in enumerate(rewards):
-            level_results[level]["episode_data"] = {"reward": episode_reward, "trajectory": episode_trajectories[i], "info": episode_info[i]}
+        # level_results[level] = {"trajectories": copy.deepcopy(all_trajectories),
+        #                         "actions": copy.deepcopy(all_actions),
+        #                         "rewards": copy.deepcopy(all_rewards),
+        #                         "action_distributions": copy.deepcopy(all_action_distributions)}
+        # print(f"\tMean reward is {mean_reward}, bk2 files stored in {record_path}.\n")
+        # level_results[level] = {"mean_reward": mean_reward, "episode_data": []}
 
-    json_filepath = eval_dir + "all_eval_info.json"
+        df = pd.DataFrame.from_dict({"trajectories": all_trajectories,
+                                "actions": all_actions,
+                                "dist_rewards": all_dist_rewards,
+                                "score_rewards": all_score_rewards,
+                                "combined_rewards": all_combined_rewards,
+                                "action_distributions": all_action_distributions})
 
-    with open(json_filepath, "w") as json_file:
-        print(f"All evaluation information being saved in {json_filepath}.\n")
-        json.dump(level_results, json_file)
+        df_filename = model_path[:-4] + f"/{level}_dataframe.pkl"
+        if not df.empty:
+            print(f"{df_filename} being saved with {len(df.index)} rows of data.")
+            df.to_pickle(df_filename) 
+        else:
+            print(f"{df_filename} not saved as dataframe is empty.")
+
+    return 
+    #     for i, episode_reward in enumerate(rewards):
+    #         level_results[level]["episode_data"].append({
+    #             "reward": episode_reward, 
+    #             "trajectory": episode_trajectories[i], 
+    #             "info": episode_info[i],
+    #             "actions": episode_actions[i]})
+
+    # json_filepath = eval_dir + "all_eval_info.json"
+
+    # try:
+    #     with open(json_filepath, "w") as json_file:
+    #         print(f"All evaluation information being saved in {json_filepath}.\n")
+    #         json.dump(level_results, json_file)
+    # except: 
+    #     backup_filepath = eval_dir + "all_eval_info.pkl"
+    #     with open(backup_filepath, "wb") as pkl_file:
+    #         print(f"All evaluation information being saved in {backup_filepath}.\n")
+    #         pickle.dump(level_results, pkl_file)
     
     return level_results
 
@@ -458,13 +583,50 @@ def main():
     env.close()
 
 
-
 def run_evaluations(saved_model_dir):
 
     env = gym.make('MarioEnv-v0')
 
-    for filename in os.listdir(saved_model_dir):
-        if filename.endswith(".zip"):
+    evaluated_files_filepath = f"{saved_model_dir}read_files_list.txt"
+    concurrent_evaluations_filepath = f"{saved_model_dir}currently_being_evaluated.txt"
+
+    if os.path.isfile(evaluated_files_filepath):
+        # load in list of all files already evaluated?
+        with open(evaluated_files_filepath, "r") as f:
+            evaluated_files_list = f.read()
+        
+        evaluated_files_list = evaluated_files_list.split("\n")
+    else:
+        evaluated_files_list = []
+
+    print(evaluated_files_list)
+
+    full_file_list = os.listdir(saved_model_dir)
+
+    file_list = []
+    for filename in full_file_list:
+        if filename.endswith(".zip") and not filename.endswith("bk2s.zip"):
+            file_list.append(filename)
+    
+    # file_list = 
+    file_list.sort()
+
+    file_list = file_list[INDEX:] if INDEX < len(file_list) else file_list
+
+    print(file_list)
+
+    for filename in file_list:
+        if os.path.isfile(concurrent_evaluations_filepath):
+            with open(concurrent_evaluations_filepath, "r") as f:
+                concurrent_evaluations_list = f.read()
+        else:
+            concurrent_evaluations_list = []
+
+        if filename not in evaluated_files_list and filename not in concurrent_evaluations_list:
+
+            with open(concurrent_evaluations_filepath, "a") as f:
+                f.write(f"{filename}\n")
+
             model_path = saved_model_dir + filename
             if "PPO" in filename:
                 model_class = PPO
@@ -477,10 +639,27 @@ def run_evaluations(saved_model_dir):
             get_model_results(model_path, model_class, env)
             print()
 
+            with open(evaluated_files_filepath, "a") as f:
+                f.write(f"{filename}\n")
+
+            # # Define directory and output zip filename
+            # dir_name = model_path.split(".")[0] # remove the .zip bit
+            # zip_name = f"{dir_name}_bk2s"
+
+            # # Create zip archive
+            # shutil.make_archive(zip_name, 'zip', dir_name)
+
+            # # Verify that ZIP was created, then delete the directory
+            # if os.path.exists(f"{zip_name}.zip"):
+            #     shutil.rmtree(dir_name, ignore_errors=True)  # Removes the entire directory
+            #     print(f"Zipped and removed: {dir_name}")
+            # else:
+            #     print("Error: Zip creation failed, directory not deleted.")
+
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("USAGE: python evaluate_agents.py <exp ID>")
+    if len(sys.argv) != 3:
+        print("USAGE: python evaluate_agents.py <exp ID> <index>")
         sys.exit(1)
 
     register(
@@ -489,8 +668,8 @@ if __name__ == "__main__":
         )
 
     EXP_RUN_ID = sys.argv[1]
+    INDEX = int(sys.argv[2])
 
     saved_model_dir = f"experiments/{EXP_RUN_ID}/saved_models/level_change_random/"
 
     run_evaluations(saved_model_dir)
-
